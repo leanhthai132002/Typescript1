2dfce1cbe16ff3ae3deff45f941ef8e225b49859
LeAnhThai <vanuong132002@gmail.com>
2022-03-09 22:06:38 +0700

    lab1

--- src/pages/student.tsx
+++ src/pages/student.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 function Student(){
     return (
-        <table>
-            <thead className="up">
-                <tr>
+        <table className="table">
+            <thead >
+                <tr className="up">
                     <th>Tên</th>
                     <th>Tuổi</th>
                     <th>SĐT</th>
@@ -12,23 +12,23 @@ function Student(){
                     <th>Xoá</th>
                 </tr>   
             </thead>
-            <tbody>
-                <tbody>
+            <tbody className="down">
                     <tr>
                         <td>Ông A</td>
                         <td>10</td>
                         <td>01023012</td>
                         <td>thailaph15128@gmail.com</td>
-                        <td>Xoá</td>
+                        <td><img src="https://picsum.photos/200" alt="" /></td>
+                        <td><button>Xoá</button></td>
                     </tr>
                     <tr>
                         <td>Ông A</td>
                         <td>10</td>
                         <td>01023012</td>
                         <td>thailaph15128@gmail.com</td>
-                        <td>Xoá</td>
+                        <td><img src="https://picsum.photos/200" alt="" /></td>
+                        <td><button>Xoá</button></td>
                     </tr>
-                </tbody>
             </tbody>
         </table>
     )
